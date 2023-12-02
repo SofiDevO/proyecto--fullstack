d555f349e8341415bc0df31cefcae431c34f1b37
SofiDevO <angiemeow89@gmail.com>
2023-12-01 23:08:55 -0600

    se corrigen erroes en header css

--- frontend/CodeJourney/src/components/NuestroServicio/NuestroServicio.tsx
+++ frontend/CodeJourney/src/components/NuestroServicio/NuestroServicio.tsx
@@ -10,9 +10,9 @@ export const NuestroServicio = () => {
       <div className="content_servicios">
 
 <div className="sepadaror">
- <h1 align="center">nuestro servicio</h1>
+ <h1 className="servicio__titulo">nuestro servicio</h1>
 
-<p align="justify">¡Bienvenido a <span>CodeJourney!</span> Aquí, la educación se adapta a ti. Imagina un espacio donde puedes explorar un mundo de conocimiento a tu propio ritmo, sin restricciones.
+<p  className="text__content" >¡Bienvenido a <span>CodeJourney!</span> Aquí, la educación se adapta a ti. Imagina un espacio donde puedes explorar un mundo de conocimiento a tu propio ritmo, sin restricciones.
 Con una amplia variedad de cursos en diversas áreas del mundo IT, desde principiantes hasta niveles avanzados, encontrarás exactamente lo que necesitas para impulsar tu aprendizaje.
 ¿Te preguntas por dónde empezar? Deja que nuestro exclusivo sistema de preferencias te guíe hacia el curso perfecto, adaptado a tus objetivos personales.
 Unete a una comunidad activa de aprendices como tú. Comparte experiencias, haz conexiones valiosas y crea tu red de conocimiento.</p>
