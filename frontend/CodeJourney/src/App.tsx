f7b1998ef5e8d0bd041c534bd358caad19d30746
DiegoRivasDev <diegodevsoftware@gmail.com>
2023-12-02 16:56:39 -0400

    404 se agrego posiblemente tiene errores

--- frontend/CodeJourney/src/App.tsx
+++ frontend/CodeJourney/src/App.tsx
@@ -4,10 +4,12 @@ import { Form } from "./components/contact-us/Form";
 import { Footer } from './components/footer/Footer';
 import { Hero } from "./components/Hero/Hero";
 import { NuestroServicio } from "./components/NuestroServicio/NuestroServicio";
+// import { SectionPageError } from "./pages/SectionPageError/SectionPageError";
 
 function App() {
   return (
     <>
+{/* <SectionPageError/> */}
       <Header />
       <Hero/>
       <NuestroServicio/>
