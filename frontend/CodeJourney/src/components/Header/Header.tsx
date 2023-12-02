c1cee2a862c633f8640f6c41437e782ca04e6c8d
SofiDevO <angiemeow89@gmail.com>
2023-12-01 22:13:54 -0600

    :ambulance: Se repara menu hamburguesa
    Se repara menu hamburguesa. Se crea componente boton, se cambia el color a el boton hamburguesa. Se crea nueva variable de color para elementos svg, filter

--- frontend/CodeJourney/src/components/Header/Header.tsx
+++ frontend/CodeJourney/src/components/Header/Header.tsx
@@ -1,39 +1,66 @@
-import { Logo } from "../Logo/Logo";
+import { useEffect } from "react";
+import {Boton} from "../Boton/Boton";
 import "./Header.css";
-export const Header = () => {
+
+export const Header = ({ menuItems }) => {
+  useEffect(() => {
+    const d = document;
+  
+    function handleClick(e) {
+      if (e.target.matches(".panel-btn") || e.target.matches(".panel-btn *")) {
+        d.querySelector(".panel").classList.toggle("is-active");
+        d.querySelector(".panel-btn").classList.toggle("is-active");
+      }
+  
+      if (e.target.matches(".header__menu-link")) {
+        d.querySelector(".panel").classList.remove("is-active");
+        d.querySelector(".panel-btn").classList.remove("is-active");
+      }
+    }
+  
+    d.addEventListener("click", handleClick);
+  
+    // Devuelve una función de limpieza que se ejecuta cuando el componente se desmonta
+    return () => {
+      d.removeEventListener("click", handleClick);
+    };
+  }, []);
+  
+
   return (
     <>
       <header className="header">
-       <Logo/>
+        <a className="header__logo" href="#">
+          <span className="header__logo--span">DevsJourney</span>
+        </a>
         <nav className="menu  panel ">
           <ul className="header__list">
-            <li className="header__item">
-              <a href="#">Nuestro servicio</a>
-            </li>
-            <li className="header__item">
-              <a href="">Caracterisiticas</a>
-            </li>
-            <li className="header__item">
-              <a href="">Contactos</a>
-            </li>
-            <li className="header__item">
-              <a href="">Nuestro equipo</a>
-            </li>
+            {menuItems.map((item, index) => (
+              <li key={index} className="header__item">
+                <a className="header__menu-link" href={item.href}>
+                  {item.text}
+                </a>
+              </li>
+            ))}
             <div className="btn__container">
-              <a href="#" className="header__btn header__btn--register">
-                Iniciar Sesión
-              </a>
-              <a href="#" className="header__btn header__btn--login">
-                Registrarse
-              </a>
+              <Boton
+                text="Iniciar Sesión"
+                className="header__btn--register"
+                href="#"
+              />
+              <Boton
+                text="Registrarse"
+                className="header__btn--login"
+                href="#"
+              />
             </div>
           </ul>
         </nav>
         <button className="hamburger hamburger--arrow panel-btn" type="button">
-      <span className="hamburger-box">
-        <span className="hamburger-inner"></span>
-      </span>
-    </button>
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
       </header>
     </>
   );
