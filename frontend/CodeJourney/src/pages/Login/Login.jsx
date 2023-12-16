92e76b0528635c6f49ba111f84cd0b4461d5edd4
SofiDevO <angiemeow89@gmail.com>
2023-12-16 02:14:26 -0600

    validacion de login

--- frontend/CodeJourney/src/pages/Login/Login.jsx
+++ frontend/CodeJourney/src/pages/Login/Login.jsx
@@ -2,53 +2,88 @@ import { Header } from "../../components/Header/Header";
 import { NavLinkEquipo } from "../../components/Navs/NavLinkEquipo";
 import "../../styles/global.css";
 import "./Login.css";
+import { redirect, NavLink as Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log(errors);
+    redirect("/dashboard");
+  };
+
   return (
     <>
       <Header navlink={<NavLinkEquipo />} />{" "}
       <main>
         <div className="login">
           <div className="form-container-login">
-            <img
-              src="../../../public/img/login/image-logo.png"
-              alt="login"
-              className="logo"
-            />
             <h1 className="title">login</h1>
-            <form action="/dashboard" className="form-login">
-              <label htmlFor="" className="label">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="form-login"
+              action="/api/save"
+            >
+              <label htmlFor="email" className="label">
                 E-MAIL
               </label>
               <input
-                type="email"
                 id="email"
+                type="email"
                 placeholder="@"
                 className="input input-user"
-                required
+                {...register("email", {
+                  required: true,
+                  pattern:
+                    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/i,
+                })}
               />
-
-              <label htmlFor="" className="label">
+              {errors.email?.type === "required" && (
+                <span className="helper__text helper__text--warning">
+                  El correo es requerido
+                </span>
+              )}
+              <label htmlFor="password" className="label">
                 PASSWORD
               </label>
               <input
                 type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 8,
+                  min: 26,
+                  pattern:
+                    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/i,
+                })}
                 id="password"
                 placeholder="***********"
                 className="input input-password"
-                required
               />
-
-              <a href="/#">
-                {/* Forgot my password */}
-                <input
-                  type="submit"
-                  value="Login"
-                  className="primary-button login-button"
-                />
-              </a>
+              {errors.password?.type === "required" && (
+                <span className="helper__text helper__text--warning">
+                  El password es requerido
+                </span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span className="helper__text helper__text--alert">
+                  Mínimo de 8 carácteres
+                </span>
+              )}
+              <input
+                type="submit"
+                value="Login"
+                className="primary-button login-button"
+              />
             </form>
-            <button className="secondary-button signup-button">Sign up</button>
+            <Link className="secondary-button signup-button" to="/registro">
+              Sign up
+            </Link>
           </div>
         </div>
       </main>
