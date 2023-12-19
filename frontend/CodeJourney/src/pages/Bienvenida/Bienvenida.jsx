13d0e519e2231f8d94055c738cf59b800e42ecb5
SofiDevO <angiemeow89@gmail.com>
2023-12-19 00:00:04 -0600

    Se agrega stepper

---
+++ frontend/CodeJourney/src/pages/Bienvenida/Bienvenida.jsx
@@ -0,0 +1,31 @@
+import { Header } from "../../components/Header/Header";
+import { NavLinkEquipo } from "../../components/Navs/NavLinkEquipo";
+import CustomizedSteppers from "../../components/Stepper/Stepper";
+import "./Bienvenida.css";
+export const Bienvenida = () => {
+  return (
+    <>
+      <Header navlink={<NavLinkEquipo />} />
+      <main>
+        <section className="welcome">
+          <h3 className="welcome__title">
+            Te damos la Bienvenida{" "}
+            {/* <span className="welcome__span">${nombre}</span> */}
+          </h3>
+          <div className="container__rutas">
+            <aside className="side__steps">
+              <CustomizedSteppers step={1} />
+            </aside>
+            <div className="caja__preferencias">
+              <p>
+                antes de empezar, marque el contenido que le gustaria que le
+                gustaria que le muestren
+              </p>
+              .
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
