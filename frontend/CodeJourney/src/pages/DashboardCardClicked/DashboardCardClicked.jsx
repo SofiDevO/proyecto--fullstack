13abfb9394a105856d1f0b948d249510798152b1
SofiDevO <angiemeow89@gmail.com>
2023-12-22 19:09:58 -0600

    fixing

--- frontend/CodeJourney/src/pages/DashboardCardClicked/DashboardCardClicked.jsx
+++ frontend/CodeJourney/src/pages/DashboardCardClicked/DashboardCardClicked.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
-import { apiUrl } from "../services/apiConfig";
+import { apiUrl } from "../../components/services/apiConfig";
 import { useLocation } from "react-router-dom";
 import "./DashboardCardClicked.css";
 import { NavLink as Link } from "react-router-dom";
