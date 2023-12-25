5b09bef585333a2e623bb2f7972d34ab9618e27f
SofiDevO <angiemeow89@gmail.com>
2023-12-24 22:27:34 -0600

    Se corrigen errores de enrutamiento

--- frontend/CodeJourney/src/pages/DashboardCardClicked/DashboardCardClicked.jsx
+++ frontend/CodeJourney/src/pages/DashboardCardClicked/DashboardCardClicked.jsx
@@ -1,73 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { NavLink as Link } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
-import "./DashboardCardClicked.css";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { apiUrl } from "../../components/services/apiConfig";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import "./DashboardCardClicked.css";
 
 export const DashboardCardClicked = () => {
   const location = useLocation();
   const etapa = location.state.etapa;
-  const [dataCompleta, setDataCompleta] = useState({});
-  const [contenido, setContenido] = useState({});
   const [token, setToken] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Obtener el token de la cookie
-    const savedToken = Cookies.get("miToken");
-
-    if (savedToken) {
-      setToken(savedToken);
-    }
-
     const fetchData = async () => {
       try {
-        const apiUrlWithEndpoint = `${apiUrl}/api/v1/contenido/obtener`;
-        console.log("URL de la API:", apiUrlWithEndpoint);
+        const savedToken = Cookies.get("miToken");
 
-        const rutasResponse = await axios.get(apiUrlWithEndpoint, {
-          headers: {
-            Authorization: `Bearer ${savedToken}`,
-          },
-        });
-
-        if (rutasResponse.status === 200) {
-          const data = rutasResponse.data;
-          setDataCompleta(data);
-
-          const contenidoEtapa = data.find(
-            (item) => item.etapa.id === etapa.id
-          );
-
-          if (contenidoEtapa) {
-            setContenido(contenidoEtapa);
-          } else {
-            console.error("No se encontró información para la etapa actual");
-          }
-        } else if (rutasResponse.status === 403) {
-          console.error(
-            "Permiso denegado. Verifica los permisos en el servidor."
-          );
-        } else {
-          console.error(
-            "Error en la respuesta del servidor:",
-            rutasResponse.status,
-            rutasResponse.statusText
-          );
+        if (!savedToken) {
+          navigate("/login");
+          return;
         }
+
+        setToken(savedToken);
       } catch (error) {
         console.error("Error al obtener la data del servidor", error);
+
+        if (error.response) {
+          console.error("Response data:", error.response.data);
+          console.error("Response status:", error.response.status);
+          console.error("Response headers:", error.response.headers);
+        }
       }
     };
 
     fetchData();
-  }, [etapa.id]);
+  }, [navigate]);
 
-  const setCookie = () => {
-    const tokenValue = "yourTokenValueHere";
-    Cookies.set("miToken", tokenValue, { expires: 1 });
-    setToken(tokenValue);
+  const handleStartLearning = () => {
+    navigate(`/ruta/${etapa.id}`);
+  };
+
+  const handleGoBack = () => {
+    navigate("/dashboard");
   };
 
   return (
@@ -75,21 +50,23 @@ export const DashboardCardClicked = () => {
       <div className="dashboard-card-clicked-border">
         <div className="dashboard-card-clicked">
           <div className="dashboard-card-clicked-img">
+            <IoMdArrowRoundBack
+              onClick={handleGoBack}
+              style={{
+                color: "#fff",
+                fontSize: "50px",
+                paddingBottom: "20px",
+              }}
+            />
             <div className="dashboard-card-clicked-container">
               <h2 className="dashboard-card-clicked-title">{etapa.nombre}</h2>
-
-              {/* Renderizar la descripción del contenido */}
-              <p className="text__content">{contenido.descripcion}</p>
-
               <p className="text__content requerid">
                 <span>{etapa.descripcion}</span>
               </p>
               <div className="dashboard-card-clicked-botton">
-                <Link to={`/ruta/`}>
-                  <button onClick={setCookie} className="card-botton">
-                    Empezar ruta de aprendizaje
-                  </button>
-                </Link>
+                <button onClick={handleStartLearning} className="card-botton">
+                  Empezar ruta de aprendizaje
+                </button>
               </div>
             </div>
           </div>
